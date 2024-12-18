02e7f37a308827f000b7fb50d4acc6f39f52e949
Andrew Waldis <andrew@andrewwaldis.com>
2024-12-17 19:29:37 -0500

    Add more mock history items

--- getMockHistoryItems.js
+++ getMockHistoryItems.js
@@ -5,13 +5,17 @@
  * @returns {Array} An array of mock history items.
  */
 export function getMockHistoryItems() {
-    const baselineDate = new Date("2024-12-14T12:00:00Z");
+    const baselineDate = new Date("2024-12-14T15:00:00Z");
     const baselineTime = baselineDate.getTime();    
     return [
       { url: "https://example.com",    title: "Example Domain",  lastVisitTime: baselineTime - 60 * 1000 },
       { url: "https://mozilla.org",    title: "Mozilla",         lastVisitTime: baselineTime - 120 * 1000 },
-      { url: "https://wikipedia.org",  title: "Wikipedia",       lastVisitTime: baselineTime - 180 * 1000 },
-      { url: "https://github.com",     title: "GitHub",          lastVisitTime: baselineTime - 240 * 1000 }
+      { url: "https://en.wikipedia.org/wiki/Kolmogorov_complexity",  title: "Kolmogorov complexity - Wikipedia", lastVisitTime: baselineTime - 180 * 1000 },
+      { url: "https://github.com/awaldis",     title: "GitHub",          lastVisitTime: baselineTime - 238 * 1000 },
+      { url: "https://developer.mozilla.org/en-US/",     title: "MDN Web Docs",          lastVisitTime: baselineTime - 342 * 1000 },
+      { url: "https://dev.to/tentanganak/7-habits-that-programmers-must-have-1dfj", title: "7 Habits that Programmers Must Have! - DEV Community",          lastVisitTime: baselineTime - 403 * 1000 },
+      { url: "https://news.ycombinator.com/news",     title: "Hacker News",          lastVisitTime: baselineTime - 478 * 1000 },
+      { url: "https://platform.openai.com/docs/api-reference/embeddings",     title: "API Reference - OpenAI API",          lastVisitTime: baselineTime - 578 * 1000 }
     ];
   }
   
